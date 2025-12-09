cb14930117a051e78527f4a24412b676f065f02d
Rahim-101 <AS3903@live.mdx.ac.uk>
2025-12-09 20:30:33 +0400

    add static file middleware to serve lesson images

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
@@ -20,6 +21,9 @@ app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
 });
+
+// Static file middleware - serve images
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // Root route
 app.get('/', (req, res) => {
